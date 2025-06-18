6d4efdd8272e74cbd9d163f526e54b8287f03c39
Hseok-2 <dookie289@gmail.com>
2025-06-18 23:43:50 +0900

    [Bronze I] Title: 팰린드롬수, Time: 108 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            String input = br.readLine();
+            boolean isPalindrome = true;
+
+            if (input.equals("0")) break;
+
+            for (int i = 0; i < input.length()/2; i++) {
+                if(input.charAt(i) != input.charAt(input.length()-i-1)) {
+                    isPalindrome = false;
+                }
+            }
+            if (isPalindrome) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+}
