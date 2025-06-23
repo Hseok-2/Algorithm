8d2a15bfa957e6bc30091d749fef7115be910ce4
Hseok-2 <dookie289@gmail.com>
2025-06-23 21:53:23 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,33 +12,34 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine()); // 사람 수
         int[] time = new int[N]; // 각 사람별 인출 시간
-        int sum = 0; // 시간 합
-        int total = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        // 배열 초기화
+        arrayFill(time, st);
+
+        // 오름차순 정렬
+        Arrays.sort(time);
+
+        // 출력
+        System.out.println(getSum(time));
+    }
+
+    // 배열 초기화
+    private static void arrayFill(int[] time, StringTokenizer st) {
         for (int i = 0; i < time.length; i++) {
             time[i] = Integer.parseInt(st.nextToken());
         }
+    }
 
-        // 오름차순 정렬
-        for (int i = 0; i < time.length - 1; i++) {
-            for (int j = i + 1; j < time.length; j++) {
-                int temp = 0;
-                if (time[i] > time[j]) {
-                    temp = time[i];
-                    time[i] = time[j];
-                    time[j] = temp;
-                }
-            }
-        }
+    // 합 계산
+    static int getSum(int[] time) {
+        int sum = 0; // 시간 합
+        int total = 0;
 
-        // 합 계산
-        for (int i = 0; i < time.length; i++) {
-            sum += time[i]; // sum = sum + time[i]
+        for (int i : time) {
+            sum += i;
             total += sum;
         }
-        
-        System.out.println(total);
+
+        return total;
     }
-}
+ }
