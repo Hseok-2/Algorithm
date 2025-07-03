68ba6994129d54e63b7bc5205b2653097244bbce
Hseok-2 <dookie289@gmail.com>
2025-07-03 14:39:37 +0900

    [Silver V] Title: 숫자 카드, Time: 840 ms, Memory: 139576 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -9,6 +9,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        HashSet<Integer> set = getCards(br);
+        int[] input = getInput(br);
+
+        System.out.println(result(set, input));
+    }
+
+     private static HashSet<Integer> getCards(BufferedReader br) throws IOException {
         int N = Integer.parseInt(br.readLine());
 
         HashSet<Integer> set = new HashSet<>(); // 상근이 카드
@@ -16,21 +24,27 @@ public class Main {
         for (int i = 0; i < N; i++) {
             set.add(Integer.parseInt(st.nextToken()));
         }
+        return set;
+    }
 
+    private static int[] getInput(BufferedReader br) throws IOException {
         int M = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
         int[] input = new int[M];
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<input.length; i++) {
             input[i] = Integer.parseInt(st.nextToken());
         }
+        return input;
+    }
 
-        for (int i = 0; i < input.length; i++) {
-            if(set.contains(input[i])) {
-                System.out.print(1 + " ");
-            }else {
-                System.out.print(0 + " ");
-            }
+    private static String result(HashSet<Integer> set, int[] arr) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int num : arr) {
+            sb.append(set.contains(num) ? "1 " : "0 ");
         }
-
+        return sb.toString().trim();
     }
 }
