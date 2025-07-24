c9dc54aa7698fc8f21570067715cc31fd7352e3a
Hseok-2 <dookie289@gmail.com>
2025-07-25 00:18:59 +0900

    [level 2] Title: 올바른 괄호, Time: 17.17 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,20 +1,21 @@
 import java.util.*;
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack();
-        
+        ArrayDeque<Character> stack = new ArrayDeque();
+
         char[] a = s.toCharArray();
         for(char c : a) {
             if (c == '(') {
                 stack.push(c);
             }
             else {
-                if(stack.isEmpty() || stack.pop() == c) {
+                if(stack.isEmpty()) {
                     return false;
                 }
+                stack.pop();
             }
         }
-        
+
         return stack.isEmpty();
     }
 }
