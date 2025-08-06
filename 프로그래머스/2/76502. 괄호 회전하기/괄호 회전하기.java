94d6864e4b3276b7f2521c6296b2503195fbf942
Hseok-2 <dookie289@gmail.com>
2025-08-07 00:12:48 +0900

    [level 2] Title: 괄호 회전하기, Time: 48.36 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,35 +1,33 @@
 import java.util.*;
 class Solution {
     public int solution(String s) {
-        int cnt = 0; // 카운트
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
         int n = s.length();
+        s += s;
+        int answer = 0;
 
+        A:
         for (int i = 0; i < n; i++) {
-            String str = s.substring(i) + s.substring(0, i);
+            ArrayDeque<Character> stack = new ArrayDeque<>();
+            for (int j = i; j < i + n; j++) {
+                char c = s.charAt(j);
 
-            Stack<Character> stack = new Stack<>();
-            boolean isValid = true;
-            char[] ch = str.toCharArray();
-            for (char c : ch) {
-                if (c == '(' || c == '{' || c == '[') {
+                if (!map.containsKey(c)) {
                     stack.push(c);
-                } else {
-                    if (stack.isEmpty()) {
-                        isValid = false;
-                        break;
-                    }
-                    char top = stack.pop();
-                    if(!((top == '(' && c == ')') || (top == '{' && c == '}') || (top == '[' && c == ']'))) {
-                        isValid = false;
-                        break;
+                }else {
+                    if (stack.isEmpty() || !stack.pop().equals(map.get(c))) {
+                        continue A;
                     }
                 }
             }
-
-            if (isValid && stack.isEmpty()) {
-                cnt++;
+            if (stack.isEmpty()) {
+                answer++;
             }
         }
-        return cnt;
+        return answer;
     }
 }
