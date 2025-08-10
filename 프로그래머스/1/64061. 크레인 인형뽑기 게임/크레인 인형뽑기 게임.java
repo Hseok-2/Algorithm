2937d7aa4a041a6a88ab8a6a92d34162cc5475a6
Hseok-2 <dookie289@gmail.com>
2025-08-10 22:17:42 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.23 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,33 +1,33 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        Stack<Integer>[] lines = new Stack[board[0].length];
-
-        // 각 열 stack 생성
-        for (int i = 0; i < board.length; i++) {
-            lines[i] = new Stack();
+       // 스택 선언
+        Stack<Integer>[] lines = new Stack[board.length];
+        for (int i = 0; i < lines.length; i++) {
+            lines[i] = new Stack<>();
         }
 
-        // 각 스택에 인형 쌓기
-        for (int i = board.length - 1; i >= 0; i--) {
-            for (int j = 0; j < board[0].length; j++) {
+        // 스택 쌓기(스택의 특성 생각해서 역순으로)
+        for (int i = lines.length - 1; i >= 0; i--) {
+            for (int j = 0; j < lines.length; j++) {
                 if (board[i][j] > 0) {
                     lines[j].push(board[i][j]);
                 }
             }
         }
 
-        Stack<Integer> bucket = new Stack<>(); // 바구니
-        int cnt = 0; // 사라진 인형의 개수
+        // 바구니 선언
+        Stack<Integer> bucket2 = new Stack<>();
+        int cnt = 0; // 제거한 인형 수
 
-        for(int move : moves) {
-            if(!lines[move-1].isEmpty()) {
+        for (int move : moves) {
+            if (!lines[move - 1].isEmpty()) {
                 int doll = lines[move-1].pop();
-                if(!bucket.isEmpty() && doll == bucket.peek()) {
-                    bucket.pop();
-                    cnt += 2;
-                } else {
-                    bucket.push(doll);
+                if(!bucket2.isEmpty() && bucket2.peek() == doll){
+                    bucket2.pop();
+                    cnt+=2;
+                }else {
+                    bucket2.push(doll);
                 }
             }
         }
