b705435275b6ba49665d8451eaadab1cc913e1cc
Hseok-2 <dookie289@gmail.com>
2025-08-14 00:01:15 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 144 ms, Memory: 17748 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -4,9 +4,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int k = Integer.parseInt(input[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
         System.out.println("<" + solution(n, k) + ">");
     }
