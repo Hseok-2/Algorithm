9a469e3d5defadd2adcfae7d5eeb4dc76650419f
Hseok-2 <dookie289@gmail.com>
2025-08-31 15:34:48 +0900

    [Silver II] Title: 최소 힙, Time: 292 ms, Memory: 26292 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -11,20 +11,22 @@ public class Main {
 
         // 우선순위 큐 선언
         PriorityQueue<Integer> q = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < n; i++) {
             int x = Integer.parseInt(br.readLine());
 
             if(x == 0) {
                 if (q.isEmpty()) {
-                    System.out.println(0);
+                    sb.append(0).append("\n");
                 } else { // x가 0이라면 가장 작은 값 출력 후 queue에서 삭제
-                    System.out.println(q.poll());
+                    sb.append(q.poll()).append("\n");
                 }
             } else {
                 q.add(x);
             }
         }
+        System.out.println(sb.toString());
     }
 
 }
