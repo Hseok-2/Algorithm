4a6dad07c3109317de3e7c756b5ba0cb027ee13d
Hseok-2 <dookie289@gmail.com>
2025-09-19 18:11:37 +0900

    [Silver IV] Title: 보물, Time: 100 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -12,41 +12,32 @@ public class Main {
         int minSum = 0; // 최소값
 
         // 배열A, B 초기화
-        int[] A = initArray(N, br);
-        int[] B = initArray(N, br);
+        int[] A = initArr(N, br);
+        int[] B = initArr(N, br);
 
-        // ArrayList 생성
-        ArrayList<Integer> listA = list(A);
-        ArrayList<Integer> listB = list(B);
+        // 배열 A 오름차순 정렬
+        Arrays.sort(A);
 
-        Collections.sort(listA); // listA 오름차순
-        listB.sort(Collections.reverseOrder()); // listB 내림차순
+        ArrayList<Integer> arrB = new ArrayList<>();
+        for(int n : B) {
+            arrB.add(n);
+        }
 
-        for(int i=0; i<N; i++) {
-            minSum += listA.get(i) * listB.get(i);
+        for (int i = 0; i < N; i++) {
+            int max = Collections.max(arrB);
+            minSum += A[i] * max;
+            arrB.remove(Integer.valueOf(max));
         }
 
         System.out.println(minSum);
     }
 
-    // 배열 초기화 메서드
-    public static int[] initArray(int n, BufferedReader br) throws IOException {
-        int[] arr = new int[n];
+    public static int[] initArr(int num, BufferedReader br) throws IOException {
+        int[] array = new int[num];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < num; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
         }
-        return arr;
+        return array;
     }
-
-    // ArrayList 선언하고 배열 원소값 복사
-    public static ArrayList<Integer> list(int[] arr) {
-        ArrayList<Integer> list = new ArrayList<>();
-
-        for(int n : arr) {
-            list.add(n);
-        }
-        return list;
-    }
-
 }
