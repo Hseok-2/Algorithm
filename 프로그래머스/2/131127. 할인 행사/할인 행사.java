d5129527380eced7601a802c478e4aac4b87fde1
Hseok-2 <dookie289@gmail.com>
2025-10-13 18:00:22 +0900

    [level 2] Title: 할인 행사, Time: 64.75 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,32 +1,27 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        HashMap<String, Integer> wantMap = new HashMap<>();
-
-        // 원하는 제품 hashMap에 초기화
-        for (int i = 0; i < want.length; i++) {
-            wantMap.put(want[i], number[i]);
+        int cnt = 0; // 회원 등록 날짜 일수 반환
+        HashMap<String, Integer> hope = new HashMap<>();
+        // 원하는 제품 HashMap으로 초기화
+        for (int i = 0; i < number.length; i++) {
+            hope.put(want[i], number[i]);
         }
-
-        int register = 0; // 회원 등록 총 일수 계산
 
         for (int i = 0; i < discount.length - 9; i++) {
-            HashMap<String, Integer> discountMap = new HashMap<>();
-
-            // 원하는 제품이 할인 품목에 있다면 discountMap에 저장
+            HashMap<String, Integer> eventProduct = new HashMap<>();
+            // 10일간 할인 행사 하는 제품 저장하는 HashMap
             for (int j = i; j < i + 10; j++) {
-                if (wantMap.containsKey(discount[j])) {
-                    discountMap.put(discount[j], discountMap.getOrDefault(discount[j], 0) + 1);
-                }
+                eventProduct.put(discount[j], eventProduct.getOrDefault(discount[j], 0) + 1);
             }
 
-            // 원하는 제품과 마트 할인 품목이 같다면 일수 +1
-            if (wantMap.equals(discountMap)) {
-                register++;
+            if (hope.equals(eventProduct)) {
+                cnt++;
             }
         }
-        return register;
+
+        return cnt;
     }
-} 
+}
