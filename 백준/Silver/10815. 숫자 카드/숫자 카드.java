b2814e989fed282bb437a901b5489512df7d40c6
Hseok-2 <dookie289@gmail.com>
2025-10-22 23:24:20 +0900

    [Silver V] Title: 숫자 카드, Time: 828 ms, Memory: 137096 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,50 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        HashSet<Integer> set = getCards(br);
-        int[] input = getInput(br);
-
-        System.out.println(result(set, input));
-    }
-
-     private static HashSet<Integer> getCards(BufferedReader br) throws IOException {
         int N = Integer.parseInt(br.readLine());
+        Set<Integer> card = new HashSet<>();
 
-        HashSet<Integer> set = new HashSet<>(); // 상근이 카드
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            set.add(Integer.parseInt(st.nextToken()));
+            card.add(Integer.parseInt(st.nextToken()));
         }
-        return set;
-    }
 
-    private static int[] getInput(BufferedReader br) throws IOException {
-        int M = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-
-        int[] input = new int[M];
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<input.length; i++) {
-            input[i] = Integer.parseInt(st.nextToken());
-        }
-        return input;
-    }
-
-    private static String result(HashSet<Integer> set, int[] arr) {
         StringBuilder sb = new StringBuilder();
 
-        for(int num : arr) {
-            sb.append(set.contains(num) ? "1 " : "0 ");
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            if (card.contains(Integer.parseInt(st.nextToken()))) {
+                sb.append(1).append(" ");
+            } else {
+                sb.append(0).append(" ");
+            }
         }
-        return sb.toString().trim();
+
+        System.out.println(sb);
+        br.close();
+
     }
 }
+
