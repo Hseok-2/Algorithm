9a20dffd975044e547a1e9f82684bf48c57b6dd9
Hseok-2 <dookie289@gmail.com>
2025-11-20 13:30:49 +0900

    [level 3] Title: 기지국 설치, Time: 1.09 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/README.md
+++ 프로그래머스/3/12979. 기지국 설치/README.md
@@ -1,10 +1,10 @@
 # [level 3] 기지국 설치 - 12979 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979?language=java) 
 
 ### 성능 요약
 
-메모리: 54.3 MB, 시간: 1.20 ms
+메모리: 54.5 MB, 시간: 1.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 19일 20:30:39
+2025년 11월 20일 13:30:44
 
 ### 문제 설명
 
--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,18 +1,18 @@
 class Solution {
     public int solution(int n, int[] stations, int w) {
-        int answer = 0; // 최소 기지국 설치 수
+        int idx = 0; // 기지국 설치 위치 인덱스
         int location = 1; // 현재 위치
-        int idx = 0; // 기지국 인덱스
+        int count = 0; // 기지국 증설 수
 
-        while(location <= n) {
-            if(idx < stations.length && location >= stations[idx]-w) { // 기지국이 설치된 위치에 도달한 경우
+        while (location <= n) {
+            if (idx < stations.length && location >= stations[idx] - w) { // 해당 기지국 인덱스 도달 범위보다 크거나 같다면
                 location = stations[idx] + w + 1;
                 idx++;
-            } else { // 기지국 설치해야하는 경우
-                location += 2 * w + 1;
-                answer++;
+            } else { // 그렇지 않고 증설해야 한다면
+                count++; // 증설 +1
+                location += 2 * w + 1; // 해당 기지국 인덱스 도달 범위보다 +1
             }
         }
-        return answer;
+        return count;
     }
 }
