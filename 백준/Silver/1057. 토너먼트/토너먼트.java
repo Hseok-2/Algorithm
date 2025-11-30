b8f8f3ad482eb6515efa83c7a2d201154e9be028
Hseok-2 <dookie289@gmail.com>
2025-11-30 15:52:43 +0900

    [Silver IV] Title: 토너먼트, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int round = 0;
+
+        for (round = 0; a != b; round++) {
+            a = (a+1)/2;
+            b = (b+1)/2;
+        }
+        System.out.println(round);
+    }
+}
