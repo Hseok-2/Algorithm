0ccdf38f55409757ba401bdd1cbd90f8f094f516
Hseok-2 <dookie289@gmail.com>
2025-12-03 00:54:03 +0900

    [Bronze II] Title: 숫자, Time: 236 ms, Memory: 24320 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+
+        if(A > B) {
+            long tmp = A;
+            A = B;
+            B = tmp;
+        }
+
+        if(A == B) {
+            System.out.println(0);
+        }else {
+            sb.append(B-A-1).append("\n");
+            for(long i=A+1; i<B; i++) {
+                sb.append(i).append(" ");
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
