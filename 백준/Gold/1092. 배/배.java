c1e1abc08235a1faa42d99e8422ec66121018d5a
Hseok-2 <dookie289@gmail.com>
2026-01-27 14:43:08 +0900

    [Gold V] Title: 배, Time: 288 ms, Memory: 16504 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -1,52 +1,58 @@
+
 import java.util.*;
 import java.io.*;
-
 import static java.util.Collections.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine()); // 크레인 개수
-        // 각 크레인별 무게 제한 초기화
-        ArrayList<Integer> craneList = listInit(N, br); // 각 크레인 무게 제한
 
+        // 각 크레인별 무게 제한 리스트
+        ArrayList<Integer> craneList = listInit(N, br);
 
         int M = Integer.parseInt(br.readLine()); // 박스 개수
-        // 각 박스별 무게 제한 초기화
+        // 각 상자별 무게 리스트
         ArrayList<Integer> boxList = listInit(M, br);
 
-        // 내림차순 정렬
+        // 크레인 리스트 내림차순 정렬
         craneList.sort(reverseOrder());
+        // 박스 리스트 내림차순 정렬
         boxList.sort(reverseOrder());
 
-        int time = 0; // 최소 시간 변수
-
-        if(craneList.get(0) < boxList.get(0)) {
+        // 가장 무거운 박스가 크레인 무게제한보다 높다면 -1 출력
+        if(boxList.get(0) > craneList.get(0)) {
             System.out.println(-1);
             return;
         }
-        // 박스의 개수가 0일때 까지 반복
-        while (!boxList.isEmpty()) {
-            for (int i = 0; i < craneList.size(); i++) {
 
-                for (int j = 0; j < boxList.size(); j++) {
-                    // 현재 크레인이 현재 박스를 들 수 있다면
-                    if(craneList.get(i) >= boxList.get(j)) {
-                        boxList.remove(boxList.get(j));
-                        break;
-                    }
+        int time = 0; // 최소 시간
+        while (!boxList.isEmpty()) {
+            int boxIdx = 0; // 박스 인덱스
+            int craneIdx = 0; // 크레인 인덱스
+
+            while (craneIdx < N) {
+                // 현재 크레인이 boxList 끝까지 순회해도 들 수 없다면 멈추고 다음 크레인으로
+                if (boxIdx == boxList.size()) break;
+
+                if(boxList.get(boxIdx) <= craneList.get(craneIdx)) { // 현재 크레인이 들 수 있는 박스 무게라면
+                    boxList.remove(boxIdx); // 해당 박스 삭제 (최신화)
+                    craneIdx++;
+                } else {
+                    boxIdx++; // 다음 박스로
                 }
             }
-            time++;
+            time++; // 한 턴 끝나면 1분 증가
         }
+
         System.out.println(time);
     }
 
-    public static ArrayList<Integer> listInit(int n, BufferedReader br) throws IOException {
+    public static ArrayList<Integer> listInit(int num, BufferedReader br) throws IOException {
         ArrayList<Integer> list = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < num; i++) {
             int weight = Integer.parseInt(st.nextToken());
             list.add(weight);
         }
