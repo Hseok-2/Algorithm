58dd99e82c85a14d4aa58a490bd7a18b490344f9
Hseok-2 <dookie289@gmail.com>
2026-02-06 22:20:55 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
 
 ### 성능 요약
 
-메모리: 85.6 MB, 시간: 0.07 ms
+메모리: 89.6 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 05일 23:11:31
+2026년 02월 06일 22:20:50
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,19 +1,23 @@
 import java.util.*;
-import java.io.*;
-
 class Solution {
     public int[] solution(int n, String[] words) {
         HashSet<String> usedWords = new HashSet<>();
-        char prevWord = words[0].charAt(0);
-        
-        for(int i=0; i<words.length; i++) {
-            if(usedWords.contains(words[i]) || prevWord != words[i].charAt(0)) {
-                return new int[]{(i%n)+1, (i/n)+1};
-            }
-            usedWords.add(words[i]);
-            prevWord = words[i].charAt(words[i].length()-1);
-        }
 
+        usedWords.add(words[0]);
+        for (int i = 1; i < words.length; i++) {
+
+            char prevLastChar = words[i-1].charAt(words[i-1].length()-1);
+            char curFirstChar = words[i].charAt(0);
+
+            // 앞 사람이 말한 단어의 마지막 문자로 시작하는지 확인 or 중복된 단어인지 확인
+            if(prevLastChar != curFirstChar || usedWords.contains(words[i])) {
+                int no = (i%n) + 1; // 번호
+                int st = (i/n) + 1; // 차례
+                return new int[]{no, st};
+            }
+
+            usedWords.add(words[i]);
+        }
         return new int[]{0, 0};
     }
 }
